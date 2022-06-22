16bae72e77a10160df6545e3f5de74ddc26633db
Aizhan Abenova <aizhan.zh.ab.07@gmail.com>
2022-06-21 21:43:28 -0400

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	for (int i=0 ; i<5 ; i++) {
         System.out.println("Hello, Am I a cat?");
+        System.out.println("Hello, I am a dog");
     }
     }
 }
+
